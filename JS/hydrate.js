13425cf46f087a2b6405146144759fa2049ac7ab
d <dommyv@gmail.com>
2023-10-18 23:47:37 -0700

    hydrate

---
+++ JS/hydrate.js
@@ -0,0 +1,11 @@
+
+//There will be a number indicating the amount of hours Nathan is cycling
+// It will return the number of liters that Nathan drank. If the value comes out as a decimal, it will need to be rounded to the lowest value.
+//1.5 - 1
+//I would multiply the number of hours with the amount of water that Nathan drinks in an hour of cycling. Then if the number of liters comes out to a decimal, I will round it down.
+
+//function litres(time) {
+  //return Math.floor(time * 0.5)
+//}
+
+const litres = time => Math.floor(time * 0.5)
---
+++ JS/phonewords.js
@@ -0,0 +1,25 @@
+
+
+
+function phoneWords(stringOfNums) {
+	const phoneNums = {a:'2', b:'22', c:'222', d:'3', e:'33', f:'333', g:'4', h:'44', i:'444', j:'5', k:'55', l:'555', m:'6', n:'66', o:'666', p:'7', q:'77', r:'777', s:'7777', t:'8', u:'88', v:'888', w:'9', x:'99', y:'999', z:'9999'}
+	let newMessage = []
+	let splitStr = stringOfNums.split('')
+	for (let i = 0; i < splitStr.length; i++) {
+		for (let [key, value] of Object.entries(phoneNums)) {
+			if (splitStr[i] === value) {
+				newMessage.push(key)
+			}
+		}
+	
+	}
+	return newMessage.join('')
+	
+
+}
+
+console.log(phoneWords('653'))
+console.log(phoneWords("443355555566604466690277733099966688"))
+
+
+//https://www.codewars.com/kata/635b8fa500fba2bef9189473/train/javascript
